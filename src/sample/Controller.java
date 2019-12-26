0a3e38f4803dfa033557e528fed8479573209380
abzh00 <49748373+abzh00@users.noreply.github.com>
2019-12-27 00:24:38 +0600

    Add files via upload

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -15,7 +15,6 @@ import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.event.ActionEvent;
 import javafx.stage.FileChooser;
-
 import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.IOException;
@@ -27,16 +26,12 @@ public class Controller implements Initializable {
 
     @FXML
     private ColorPicker bgcolor, lcolor, pcolor;
-
     @FXML
-    private RadioButton lradio, iradio, cradio;
-
+    private RadioButton lradio, iradio;
     @FXML
     private TextField npTF, NTF;
-
     @FXML
     private AnchorPane pane;
-
     @FXML
     private Slider slider;
 
@@ -51,7 +46,9 @@ public class Controller implements Initializable {
     private void general(){
         pane.getChildren().clear();
         Color selectedColor = bgcolor.getValue();
-        pane.setBackground(new Background(new BackgroundFill(Paint.valueOf(selectedColor.toString()), CornerRadii.EMPTY, Insets.EMPTY)));
+        pane.setBackground(new Background(new
+                BackgroundFill(Paint.valueOf(selectedColor.toString()),
+                CornerRadii.EMPTY, Insets.EMPTY)));
         lColor = lcolor.getValue();
         pColor = pcolor.getValue();
         n  = Double.parseDouble(npTF.getText());
@@ -60,11 +57,12 @@ public class Controller implements Initializable {
         k = 360/n;
         m = 765/radius;
         m1 = 765/n;
-
     }
+
     private void fun(){
         double width = 600, height = 600;
         for (int i = 0; i<n; i++){
+            System.out.println("Point " + i + " is connected with point "+ (int) ((i*N)%n)+".");
             double x = width/2 + radius*Math.sin(Math.toRadians(i*k));
             double y = height/2 + radius*Math.cos(Math.toRadians(i*k));
             double x1 = width/2 + radius*Math.sin(Math.toRadians(((i*N)%n)*k));
@@ -79,6 +77,7 @@ public class Controller implements Initializable {
     private void funl(){
         double width = 600, height = 600;
         for (int i = 0; i<n; i++){
+            System.out.println("Point " + i + " is connected with point "+ (int) ((i*N)%n)+".");
             double x = width/2 + radius*Math.sin(Math.toRadians(i*k));
             double y = height/2 + radius*Math.cos(Math.toRadians(i*k));
             double x1 = width/2 + radius*Math.sin(Math.toRadians(((i*N)%n)*k));
@@ -86,25 +85,18 @@ public class Controller implements Initializable {
             Line line = new Line(x, y, x1, y1);
             double d = Math.sqrt(Math.pow((x-x1), 2) + Math.pow((y-y1), 2))*m;
             int c = (int) (d-0.9999999999999);
-            int s = c%255;
-            Color c1;
+            int s = c%255; Color c1;
             if (0<=c & c<=255){
-                //c1 = Color.rgb(255-s,255,0);
                 c1 = Color.rgb(255, s, 0);
             }else if (255<c & c<=510){
-                //c1 = Color.rgb(0,255,s);
                 c1 = Color.rgb(255-s,255,0);
             }else if (510<c & c<=765) {
-                //c1 = Color.rgb(0, 255-s, 255);
                 c1 = Color.rgb(0,255,s);
             }else if (765<c & c<=1020) {
-                //c1 = Color.rgb(s, 0, 255);
                 c1 = Color.rgb(0, 255-s, 255);
             }else if (1020<c & c<=1275) {
-                //c1 = Color.rgb(255, 0, 255-s);
                 c1 = Color.rgb(s, 0, 255);
             }else{
-                //c1 = Color.rgb(255, s, 0);
                 c1 = Color.rgb(255, 0, 255-s);
             }
             line.setStroke(c1);
@@ -115,6 +107,7 @@ public class Controller implements Initializable {
     private void funi(){
         double width = 600, height = 600;
         for (int i = 0; i<n; i++){
+            System.out.println("Point " + i + " is connected with point "+ (int) ((i*N)%n)+".");
             double x = width/2 + radius*Math.sin(Math.toRadians(i*k));
             double y = height/2 + radius*Math.cos(Math.toRadians(i*k));
             double x1 = width/2 + radius*Math.sin(Math.toRadians(((i*N)%n)*k));
@@ -122,25 +115,18 @@ public class Controller implements Initializable {
             Line line = new Line(x, y, x1, y1);
             double d = m1*i;
             int c = (int) (d);
-            int s = c%255;
-            Color c1;
+            int s = c%255; Color c1;
             if (0<=c & c<=255){
-                //c1 = Color.rgb(255-s,255,0);
                 c1 = Color.rgb(255, s, 0);
             }else if (255<c & c<=510){
-                //c1 = Color.rgb(0,255,s);
                 c1 = Color.rgb(255-s,255,0);
             }else if (510<c & c<=765) {
-                //c1 = Color.rgb(0, 255-s, 255);
                 c1 = Color.rgb(0,255,s);
             }else if (765<c & c<=1020) {
-                //c1 = Color.rgb(s, 0, 255);
                 c1 = Color.rgb(0, 255-s, 255);
             }else if (1020<c & c<=1275) {
-                //c1 = Color.rgb(255, 0, 255-s);
                 c1 = Color.rgb(s, 0, 255);
             }else{
-                //c1 = Color.rgb(255, s, 0);
                 c1 = Color.rgb(255, 0, 255-s);
             }
             line.setStroke(c1);
@@ -210,10 +196,13 @@ public class Controller implements Initializable {
 
         fileChooser.setTitle("Save Image File");
         fileChooser.setInitialFileName("myCircle");
-        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("PNG Files", "*.png"), new FileChooser.ExtensionFilter("GIF Files", "*.gif"), new FileChooser.ExtensionFilter("JPEG Files", "*.jpeg"), new FileChooser.ExtensionFilter("BMP Files", "*.bmp"));
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("PNG Files", "*.png"),
+                new FileChooser.ExtensionFilter("GIF Files", "*.gif"),
+                new FileChooser.ExtensionFilter("JPEG Files", "*.jpeg"),
+                new FileChooser.ExtensionFilter("BMP Files", "*.bmp"));
 
         File file = fileChooser.showSaveDialog(null);
-
         ImageIO.write(SwingFXUtils.fromFXImage(image, null), "png", file);
     }
 
